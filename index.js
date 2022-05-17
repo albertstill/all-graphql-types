beb300f28c1aeaef17a7172af6475e98b8db1262
Albert Still <albertjamesstill@gmail.com>
2022-05-17 16:10:20 +1000

    .

--- index.js
+++ index.js
@@ -111,9 +111,13 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+(async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
 
-const app = express();
-server.applyMiddleware({ app });
+  const app = express();
 
-app.listen({ port: 4000 }, () => console.log(`Now browse to http://localhost:4000${server.graphqlPath}`));
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () => console.log(`Now browse to http://localhost:4000${server.graphqlPath}`));
+})();
